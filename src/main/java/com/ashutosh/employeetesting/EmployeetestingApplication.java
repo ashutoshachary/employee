7964d91519f2ed5639d6a42934ba7fdc365c70a6
ashutosh <ashutoshacharya908@gmail.com>
2025-02-14 17:14:34 +0530

    My files

--- src/main/java/com/ashutosh/employeetesting/EmployeetestingApplication.java
+++ src/main/java/com/ashutosh/employeetesting/EmployeetestingApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableScheduling
 public class EmployeetestingApplication {
+	
+	static {
+        // Load environment variables from .env
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeetestingApplication.class, args);
